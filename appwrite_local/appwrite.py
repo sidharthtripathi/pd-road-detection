84a42fec8d3472c5ca84e8fa3e096ddabe979e8b
Sidharth Tripathi <siharthtripathi807@gmail.com>
2025-08-08 00:27:00 +0530

    working mvp

--- appwrite_local/appwrite.py
+++ appwrite_local/appwrite.py
@@ -1,11 +1,14 @@
 from appwrite.client import Client
 from appwrite.services.storage import Storage
+import os
 client = Client()
-
+APPWRITE_ENDPOINT =   os.getenv("APPWRITE_ENDPOINT")
+APPWRITE_PROJECT = os.getenv("APPWRITE_PROJECT")
+APPWRITE_KEY = os.getenv("APPWRITE_KEY")
 (client
-    .set_endpoint("http://localhost/v1")  # Appwrite endpoint
-    .set_project("6894b8a8003aecf5ee16")       # Replace with your Project ID
-    .set_key("standard_e2a6310ef25297d1cde6579a559ae64e9ff320a70432faa16ab6000d5f6bd06421bf8a1e7b574b7ed8aad75406dd99078ee22a94ecd79bea5b747a3375676f912c790c31c45ecc085261a313784893fac501a5d91803f5c4ce41522e379bfcdba284bc88ca86b7b40a001c53f9022fb4493b8ff2d8d6bd77719b6d2ab7763c55")              # Replace with your API Key
+    .set_endpoint(APPWRITE_ENDPOINT)  
+    .set_project(APPWRITE_PROJECT) 
+    .set_key(APPWRITE_KEY)
 )
 
 storage = Storage(client)
--- process.py
+++ process.py
@@ -1,26 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
 import pika
 import pymongo
 import os
 import json
 from main import RoadDefectDetector
 from appwrite_local.appwrite import storage
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = mongo_client["roadvision"]
+
+mongo_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = mongo_client[os.getenv("MONGO_COLLECTION")]
 videos_collection = db["videos"]
 images_collection = db["images"]
 
 def callback(ch, method, properties, body):
     detector = RoadDefectDetector('yolo-pd.pt')
     message_data = json.loads(body.decode('utf-8'))
-    type = message_data["type"]
     id = message_data["id"]
-    file_name = storage.get_file(file_id=id, bucket_id="6894b90a002c1e18828b")["name"]
-    open(file_name, "wb").write(storage.get_file_download(file_id=id,bucket_id="6894b90a002c1e18828b"))
+    bucket_id = message_data["bucketID"]
+    file_metadata = storage.get_file(file_id=id, bucket_id=bucket_id)
+    type = "img" if file_metadata["mimeType"].startswith("image/") else "vid"
+    file_name = file_metadata["name"]
+    open(file_name, "wb").write(storage.get_file_download(file_id=id,bucket_id=bucket_id))
     if type == "img":
         result = detector.process_image(file_name)
+        result["trackingID"] = id
         images_collection.insert_one(result)
     elif type == "vid":
         result = detector.process_video(file_name)
+        result["trackingID"] = id
         videos_collection.insert_one(result)
     
     os.remove(file_name)
@@ -30,9 +37,9 @@ def callback(ch, method, properties, body):
 
 
 try:
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv("RABBIT_URI")))
     channel = connection.channel()
-    channel.basic_consume(queue='roadvision', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue="roadvision", on_message_callback=callback, auto_ack=True)
     channel.start_consuming()
 except KeyboardInterrupt:
     print("Shutting down gracefully...")
